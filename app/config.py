ffb4c5d490a7098aaa778afcf46c10aad19b936e
Kashif <kashifahmad124590@gmail.com>
2026-03-15 03:00:07 +0500

    setup docker, db adminer

---
+++ app/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_DB: str = "team_db"
+    POSTGRES_PORT: int = 5434   
+    DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5434/team_db"
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
